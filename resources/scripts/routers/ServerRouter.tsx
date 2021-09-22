2a8d62de30bf3a9c7f6dc2e2bee3dea36089e198
Harry W <hjohnw13@gmail.com>
2021-09-22 17:25:14 +0100

    Fix Discord link

--- resources/scripts/routers/ServerRouter.tsx
+++ resources/scripts/routers/ServerRouter.tsx
@@ -126,7 +126,9 @@ const ServerRouter = ({ match, location }: RouteComponentProps<{ id: string }>) 
                                 <Can action={[ 'settings.*', 'file.sftp' ]} matchAny>
                                     <NavLink to={`${match.url}/settings`}>Settings</NavLink>
                                 </Can>
-                                <NavLink to={`https://discord.gg/example`}>Discord</NavLink>
+                                <a href={'https://discord.gg/example'} rel="noreferrer" target={'_blank'}>
+                                    Discord
+                                </a>
                                 
                                 {rootAdmin &&
                                 <a href={'/admin/servers/view/' + serverId} rel="noreferrer" target={'_blank'}>
